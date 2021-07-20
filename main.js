83fc5888a93f59049fabde6543ea483e114415fa
JoaoVALdaSilva <81167465+JoaoVALdaSilva@users.noreply.github.com>
2021-07-20 17:08:34 -0300

    a

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+});
+
+var typed = new typed(".typing", {
+    string: ["Desenvolvedor", "Escrito", "Youtuber", "Editor", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
